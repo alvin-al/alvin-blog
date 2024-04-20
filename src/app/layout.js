3026006a0f6b223a40a1f615696ca9a35ef3e43f
Alvin Al <alvvxal@gmail.com>
2024-04-20 14:32:02 +0700

    Add font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,14 @@
-import { Inter } from "next/font/google";
+import { Inter, PT_Serif } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
+export const ptserif = PT_Serif({
+  subsets: ['latin'],
+  variable: '--font-ptserif',
+  weight: ["400", "700"]
+})
 
 export const metadata = {
   title: "Alvin Blog",
